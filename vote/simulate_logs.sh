0d82eaca7d4738bb5fcf9b54b326db04b3ad37f4
Ajeet Singh Raina, Docker Captain, ARM Innovator <ajeetraina@gmail.com>
2024-09-20 16:06:00 +0530

    Update simulate_logs.sh

--- vote/simulate_logs.sh
+++ vote/simulate_logs.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 while true; do
-  echo "$(date +'%Y-%m-%d %H:%M:%S') INFO: Simulated vote log message."
+  # Generate a random number between 1 and 10
+  random_number=$(( (RANDOM % 10) + 1 ))
+
+  # If the random number is 1 (10% chance), print an ERROR message
+  if [ $random_number -eq 1 ]; then
+    echo "$(date +'%Y-%m-%d %H:%M:%S') ERROR: Simulated vote error occurred."
+  else
+    # Otherwise, print an INFO message
+    echo "$(date +'%Y-%m-%d %H:%M:%S') INFO: Simulated vote log message."
+  fi
+
+  # Sleep for 2 seconds before generating the next log
   sleep 2
 done
